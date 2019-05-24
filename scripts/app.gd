2505584175a02d2c3cd4685187762a9ac13ee512
jagc <jag.cordoviz@gmail.com>
2019-05-25 05:31:50 +0800

    added functions to make menu button work

--- scripts/app.gd
+++ scripts/app.gd
@@ -1,7 +1,20 @@
 extends Control
 
 func _ready():
-	pass # Replace with function body.
+	$MenuButtonFile.get_popup().add_item("Open File")
+	$MenuButtonFile.get_popup().add_item("Save as File")
+	$MenuButtonFile.get_popup().add_item("Quit")
+	
+	$MenuButtonFile.get_popup().connect("id_pressed", self, "_on_item_pressed")
+	
+func _on_item_pressed(arg):
+	match arg:
+		0:
+			_on_openFile_pressed()
+		1:
+			_on_saveFile_pressed()
+		2:
+			_quit_app()
 
 func _process(_delta):
 	if Input.is_action_just_pressed("saveFile"):
@@ -9,7 +22,7 @@ func _process(_delta):
 	elif Input.is_action_just_pressed("openFile"):
 		_on_openFile_pressed()
 	elif Input.is_action_just_pressed("quit"):
-		get_tree().quit()
+		_quit_app()
 		
 func _on_openFile_pressed():
 	$FileDialog.popup()
@@ -30,3 +43,6 @@ func _on_saveFileDialogue_file_selected(path):
 	f.open(path, 2)
 	f.store_string($TextEdit.text)
 	f.close()
+
+func _quit_app():
+	get_tree().quit()
