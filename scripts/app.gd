4fa76a18562f69c07e26a749ec01799ae617cec4
jagc <jag.cordoviz@gmail.com>
2019-05-25 15:01:41 +0800

    major refactor of redundant code

--- scripts/app.gd
+++ scripts/app.gd
@@ -1,19 +1,32 @@
 extends Control
-# another awesome comment
+
 var appName = "Text Editor Tutorial"
 var currentFile = "Untitled"
+var menuDict = {
+	'MenuButtonFile' : ["New File", "Open File", "Save", "Save as File", "Quit"],
+	'MenuButtonHelp' : ["Godot Website", "About"]
+}
 
 func _ready():
-	_updateWindowTitle()
-	_add_menu_items('MenuButtonFile', ["New File", "Open File", "Save", "Save as File", "Quit"])
+	_set_menuScreen_objects()
 
+	_updateWindowTitle()
+
+	_set_menuScreen_shortcut_keys()
+
+func _set_menuScreen_shortcut_keys():
 	$MenuButtonFile.get_popup().set_item_shortcut(4, _set_shortcut(KEY_Q), true) # 3rd parameter tells that this shortcut is global
 
-	_add_menu_items('MenuButtonHelp', ["Godot Website", "About"])
-	
-	# signals
-	$MenuButtonFile.get_popup().connect("id_pressed", self, "_on_item_pressed", ["MenuButtonFile"])
-	$MenuButtonHelp.get_popup().connect("id_pressed", self, "_on_item_pressed", ["MenuButtonHelp"])
+func _set_menuScreen_objects():
+	# menuItem is a string
+	# menuDict[menuItem] is an array
+	for menuItem in menuDict:
+		_add_menu_items(menuItem, menuDict[menuItem])
+		_invoke_signals(menuItem)
+
+# create signals
+func _invoke_signals(menuName):
+	_set_menu_root(menuName).get_popup().connect("id_pressed", self, "_on_item_pressed", [menuName])
 
 func _add_menu_items(menuName, menuItems):
 	for item in menuItems:
@@ -25,6 +38,7 @@ func _set_menu_root(menuName):
 
 func _set_shortcut(key):
 	# alternative way of setting a shortcut key
+	# Besides using the project settings' input map
 	var shortcut = ShortCut.new()
 	var inputEventKey = InputEventKey.new()
 	inputEventKey.set_scancode(key) #if keyboard key 'Q' is pressed
