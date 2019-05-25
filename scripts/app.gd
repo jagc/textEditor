d8137c05fa07bdc76555e326ec13d62e3db38b79
jagc <jag.cordoviz@gmail.com>
2019-05-25 14:11:53 +0800

    major refactor

--- scripts/app.gd
+++ scripts/app.gd
@@ -4,48 +4,53 @@ var appName = "Text Editor Tutorial"
 var currentFile = "Untitled"
 
 func _ready():
-	updateWindowTitle()
-	$MenuButtonFile.get_popup().add_item("New File")
-	$MenuButtonFile.get_popup().add_item("Open File")
-	$MenuButtonFile.get_popup().add_item("Save")
-	$MenuButtonFile.get_popup().add_item("Save as File")
-	$MenuButtonFile.get_popup().add_item("Quit")
+	_updateWindowTitle()
+	_add_menu_items('MenuButtonFile', ["New File", "Open File", "Save", "Save as File", "Quit"])
+
+	$MenuButtonFile.get_popup().set_item_shortcut(4, _set_shortcut(KEY_Q), true) # 3rd parameter tells that this shortcut is global
+
+	_add_menu_items('MenuButtonHelp', ["Godot Website", "About"])
 	
+	# signals
+	$MenuButtonFile.get_popup().connect("id_pressed", self, "_on_item_pressed", ["MenuButtonFile"])
+	$MenuButtonHelp.get_popup().connect("id_pressed", self, "_on_item_pressed", ["MenuButtonHelp"])
+
+func _add_menu_items(menuName, menuItems):
+	for item in menuItems:
+		_set_menu_root(menuName).get_popup().add_item(item)
+
+func _set_menu_root(menuName):
+	var menu = $MenuButtonFile if menuName == 'MenuButtonFile' else $MenuButtonHelp
+	return menu
+
+func _set_shortcut(key):
 	# alternative way of setting a shortcut key
 	var shortcut = ShortCut.new()
 	var inputEventKey = InputEventKey.new()
-	inputEventKey.set_scancode(KEY_Q) #if keyboard key 'Q' is pressed
+	inputEventKey.set_scancode(key) #if keyboard key 'Q' is pressed
 	inputEventKey.control = true #if keyboard key 'Control' is pressed
 	shortcut.set_shortcut(inputEventKey) #re-sets and confirms the variables/properties of the instance set above.
+	return shortcut
 	
-	$MenuButtonFile.get_popup().set_item_shortcut(4, shortcut, true) # 3rd parameter tells that this shortcut is global
-	
-	$MenuButtonFile.get_popup().connect("id_pressed", self, "_on_item_pressed", ["MenuButtonFile"])
-	
-	$MenuButtonHelp.get_popup().add_item("Godot Website")
-	$MenuButtonHelp.get_popup().add_item("About")
-	$MenuButtonHelp.get_popup().connect("id_pressed", self, "_on_item_pressed", ["MenuButtonHelp"])
-	
-func updateWindowTitle():
+func _updateWindowTitle():
 	OS.set_window_title(appName + ' - ' + currentFile)
 	
-func newFile():
+func _newFile():
 	currentFile = "Untitled"
-	updateWindowTitle()
+	_updateWindowTitle()
 	$TextEdit.text = ''
 
+# can be another way of doing this. Besides getting id.
+# this is our implementation, for now.
 func _on_item_pressed(id, menuName):
-	# can be another way of doing this. Beside getting id.
-	# this is our implementation, for now.
-	var menu = $MenuButtonFile if menuName == 'MenuButtonFile' else $MenuButtonHelp
-	var item_name = menu.get_popup().get_item_text(id)
+	var item_name = _set_menu_root(menuName).get_popup().get_item_text(id)
 	match item_name:
 		"Open File":
 			_on_openFile_pressed()
 		"New File":
-			newFile()
+			_newFile()
 		"Save":
-			saveFile()
+			_saveFile()
 		"Save as File":
 			_on_saveFile_pressed()
 		"Quit":
@@ -69,14 +74,13 @@ func _on_openFile_pressed():
 func _on_saveFile_pressed():
 	$saveFileDialogue.popup()
 
-
 func _on_FileDialog_file_selected(path):
 	var f = File.new()
 	f.open(path, 1)
 	$TextEdit.text = f.get_as_text()
 	f.close()
 	currentFile = path
-	updateWindowTitle()
+	_updateWindowTitle()
 
 func _on_saveFileDialogue_file_selected(path):
 	var f = File.new()
@@ -84,12 +88,12 @@ func _on_saveFileDialogue_file_selected(path):
 	f.store_string($TextEdit.text)
 	f.close()
 	currentFile = path
-	updateWindowTitle()
+	_updateWindowTitle()
 
 func _quit_app():
 	get_tree().quit()
 	
-func saveFile():
+func _saveFile():
 	var path = currentFile
 	if path == 'Untitled':
 		$saveFileDialogue.popup()
@@ -98,4 +102,4 @@ func saveFile():
 		f.open(path, 2)
 		f.store_string($TextEdit.text)
 		f.close()
-		updateWindowTitle()
+		_updateWindowTitle()
